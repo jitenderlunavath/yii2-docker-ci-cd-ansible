a1166fa7d9b0a687538fbce064056695e7814efd
jitenderlunavath <jitenderlunavath@gmail.com>
2025-05-04 16:35:46 +0000

    Update composer.json and add composer.lock for PHP 7.4 compatibility

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'bDbRziGLb5okGTzUwbuvVQ_H_nWfCxTQ',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
